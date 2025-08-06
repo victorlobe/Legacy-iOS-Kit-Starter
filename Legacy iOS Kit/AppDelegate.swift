7838be7079713aa566cb867b7d1f73481f7df5f7
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2025-08-06 23:56:39 +0200

    Forgot to make the username dynamic, oops

--- Legacy iOS Kit/AppDelegate.swift
+++ Legacy iOS Kit/AppDelegate.swift
@@ -10,10 +10,14 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
+        let homePath = NSHomeDirectory() // oder: FileManager.default.homeDirectoryForCurrentUser.path
+        let scriptPath = "\(homePath)/Legacy-iOS-Kit/restore.sh"
+        
         let task = Process()
         task.launchPath = "/usr/bin/open"
-        task.arguments = ["-a", "Terminal", "/Users/victor/Legacy-iOS-Kit/restore.sh"]
+        task.arguments = ["-a", "Terminal", scriptPath]
         task.launch()
+        
         NSApp.terminate(nil)
     }
 }
